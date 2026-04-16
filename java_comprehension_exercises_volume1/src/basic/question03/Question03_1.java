7d6269262b7aff3833ab1e648741ea2965180cbb
misakita <misakita@C010423604.zipangu.cac.co.jp>
2026-04-16 13:47:07 +0900

    Question03完了しました。

--- java_comprehension_exercises_volume1/src/basic/question03/Question03_1.java
+++ java_comprehension_exercises_volume1/src/basic/question03/Question03_1.java
@@ -9,6 +9,12 @@ public class Question03_1 {
 	public static void main(String[] args) {
 
 		// TODO:ここに実装
-		
+		int price = 30;
+		int totalBudget = 300;
+		//購入予算を意味する
+		int quantity = totalBudget / price;
+		//購入個数を意味する
+		System.out.println("商品の購入数は" + quantity + "です。");
+
 	}
 }
