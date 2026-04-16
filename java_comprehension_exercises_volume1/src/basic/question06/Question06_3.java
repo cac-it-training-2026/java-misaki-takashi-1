325f7f472e960a39e2237d9e3cffbc97505d5b8f
misakita <misakita@C010423604.zipangu.cac.co.jp>
2026-04-16 13:47:55 +0900

    Question06完了しました。

--- java_comprehension_exercises_volume1/src/basic/question06/Question06_3.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_3.java
@@ -1,13 +1,37 @@
 package basic.question06;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question06_3 {
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		
-		// TODO:ここに実装
-		
-	}
 
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		// TODO:ここに実装
+		int amount = 0;
+		//鉛筆の個数
+		int price = 100;
+		//鉛筆の値段
+
+		System.out.println("鉛筆を購入しますか？");
+		System.out.println("はい：0、いいえ：1＞");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		String buyFlagString = reader.readLine();
+		int buyFlag = Integer.parseInt(buyFlagString);
+
+		while (buyFlag == 0) {
+			amount += 1;
+			System.out.println("購入する鉛筆の本数を1つ増やしますか？");
+			System.out.println("はい：0、いいえ：1");
+
+			String buyFlagString2 = reader.readLine();
+			buyFlag = Integer.parseInt(buyFlagString2);
+		}
+
+		System.out.println("購入した鉛筆の本数は" + amount + "本です。");
+
+	}
 }
--- java_comprehension_exercises_volume1/src/basic/question06/Question06_4.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_4.java
@@ -1,12 +1,36 @@
 package basic.question06;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question06_4 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		// TODO:ここに実装
+		int amountBooks = 0;
+		//本の冊数
+		int sellFlag = 0;
+		//本を売却するかの判定
+		int bookPrice = 200;
+		//ほんの値段
+		System.out.println("本を売却します");
+
+		do {
+			amountBooks += 1;
+			System.out.println("売却する本の冊数を1つ増やしますか？");
+			System.out.println("はい：0、いいえ：1");
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+			String sellFlagString = reader.readLine();
+			sellFlag = Integer.parseInt(sellFlagString);
+
+		} while (sellFlag == 0);
+
+		System.out.println("売却する本の冊数は" + amountBooks + "冊です。");
+		System.out.println("売却した本の合計金額は" + amountBooks * bookPrice + "円です。");
 
 	}
 
